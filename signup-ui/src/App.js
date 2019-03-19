94952113a4331f8c4b3094bfac504d2a833e7a9d
jawad727 <jawaddhussein@gmail.com>
2019-03-18 17:10:51 -0700

    Deployment

--- signup-ui/src/App.js
+++ signup-ui/src/App.js
@@ -3,18 +3,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Loggedin from './Loggedin'
 import authenticate from './Authenticate'
-import Login from './Login'
-import { Route } from 'react-router-dom'
+import Register from './Register'
+import { Route, Switch } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Route path="/home" render={() => <Loggedin/>} /> */}
-        <Loggedin />
+      <h1>SIGN IN</h1>
+      <Switch><Route path="/" component={Register} exact/></Switch>
       </div>
     );
   }
 }
 
-export default authenticate(App)(Login);
+export default App
