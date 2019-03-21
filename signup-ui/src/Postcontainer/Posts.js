5dfc2cc55dee49350ef157de0ab447bf8ab63d38
jawad727 <jawaddhussein@gmail.com>
2019-03-21 16:05:28 -0700

    Update button finally works

--- signup-ui/src/Postcontainer/Posts.js
+++ signup-ui/src/Postcontainer/Posts.js
@@ -5,12 +5,13 @@ import CommentSection from './CommentSection'
 class Posts extends React.Component {
     state={
 
-        singleComment: {},
-
+        singleComment: {}, //singlePost
+        comments: [],
         commentItem: {
             comment: '',
-            name: '',
-            post_id: this.props.posts.id },
+            name: ''},
+
+            post_id: this.props.posts.id ,
 
         isUpdating: false
 
@@ -26,15 +27,18 @@ class Posts extends React.Component {
 
     getSinglePost = (id) => {
         axios
-        .get(`https://disney-parents.herokuapp.com/posts/${id}/comments`, {headers: {authorization: localStorage.getItem('jwt')}})
+        .get(`https://disney-parents.herokuapp.com/posts/${id}/comments`, {
+            headers: {authorization: localStorage.getItem('jwt')}})
         .then(res => {
             
             this.setState({
-              singleComment: res.data
+                singleComment: res.data,
+              comments: res.data.comments.map(item => 
+                Object.assign({ isUpdating: false }, item))
             })
-            console.log(typeof res.data)
-            console.log(res.data.constructor === Array)
-            console.log(res.data)
+            // console.log(typeof res.data)
+            // console.log(res.data.constructor === Array)
+            // console.log(res.data)
         })
         .catch(err => {
             console.log("MOUNTING ERROR")
@@ -51,57 +55,78 @@ class Posts extends React.Component {
       populateForm = (e, id) => {
         e.preventDefault();
         this.setState({
-          commentItem: this.state.singleComment.comments.find(commentItem => commentItem.id === id),
+          commentItem: this.state.comments.find(commentItem => commentItem.id === id),
           isUpdating: true
         });
         console.log(this.state.commentItem)
       };
 
 
-      updateItem = () => {
+      updateItem = (e, id) => {
+          
+          const newObject = {
+              post_id: this.state.post_id,
+              name: 'MYNAME',
+              comment: this.state.commentItem,
+          }
+
+          
+          e.preventDefault()
+
         axios
-        .put(`https://disney-parents.herokuapp.com/comments/${this.state.post_id}`, this.state.commentItem)
+        .put(`https://disney-parents.herokuapp.com/comments/${id}`, newObject)
+
         .then(res => {
+            console.log(res.data)
             this.setState({
-                singleComment: {comments: res.data},
+                // singleComment: { comments: res.data },
                 commentItem: {
                     comment: '',
                     name: '',
-                    post_id: this.props.posts.id },
+                },
+                    post_id: this.props.posts.id ,
                     isUpdating: false
             })
+            window.location.reload()
+            
+            console.log()
         })
-        .catch(err => console.log("UPDATE_ERROR"))
+        .catch(err => console.log(err))
 
       }
 
-      ChangeHandler = (e) => {
-      this.setState({[e.target.value]: e.target.name}) }
-
+      changeHandler = (e) => {
+      this.setState({[e.target.name]: e.target.value}) }
+      
+    //   changeHandlerCommentItem = (e) => {
+    //   this.setState({[e.target.name]["comment"]: e.target.value}) }
 //============================================================================
 
      commentLoop = () => {
          
         //  console.log(this.state.singleComment.comments.constructor === Array)
-         console.log(this.state.singleComment)
+        //  console.log(this.state.singleComment)
         //  console.log(Object.keys(this.state.singleComment))
-         if (this.state.singleComment.comments){
-            console.log(this.state.singleComment.comments.constructor === Array)
+        //  if (this.state.singleComment.comments){
+        //     console.log(this.state.singleComment.comments.constructor === Array)
              
-         return this.state.singleComment.comments.map(item => {
+            const { name, id } = this.props.posts;
+
+
+         return this.state.comments.map(item => {
             return(
             
             <div>
-            <p>{item.name}</p>
+            <p>{name}</p>
             <p>{item.comment}</p>
             <button onClick={e => this.deleteComment(e, item.id)} >Delete Comment</button>
             <button onClick={e => this.populateForm(e, item.id)} >Edit Comment</button>
-            {this.state.isUpdating ? <button onClick={(e) => this.updateItem()} >UPDATE</button> : null}
-            {this.state.isUpdating ? <input name="commentItem" onChange={this.ChangeHandler} value={this.state.commentItem.comment}/> : null}
+            {this.state.isUpdating ? <button onClick={(e) => this.updateItem(e, item.id)} >UPDATE</button> : null}
+            {this.state.isUpdating ? <input name="commentItem" onChange={this.changeHandler} value={this.state.commentItem.comment}/> : null}
             </div>
             )
          })
-     }}
+     }
 
 
 
@@ -119,8 +144,7 @@ class Posts extends React.Component {
 
     render() {
 
-        const comments = this.state.singleComment.comments;
-        console.log(comments)
+        console.log(this.state.singleComment)
 
     return (
         <div>
